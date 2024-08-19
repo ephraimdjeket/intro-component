e49c9a35ce90a4bda3537e24b08dd5480e5635cb
Ephraim Djeket <djeketefra10@hotmail.com>
2024-08-19 19:20:26 +0200

    File scanner and custom styling

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "public/index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "desktop": "url('../assets/bg-intro-desktop.png')",
+        "mobile": "url('../assets/bg-intro-mobile.png')",
+      },
+      colors: {
+        "custom-pink": "#FF7979",
+        "custom-green": "#38CC8B",
+        "custom-purple": "#5E54A4",
+        "custom-black": "#3D3B48",
+      },
+      width: {
+        "form-w-desk": "540px",
+        "input-w-desk": "460px",
+        "form-w-mob": "327px",
+      },
+      height: {
+        "form-h-mob": "452px",
+        "form-h-desk": "524px",
+      },
+      fontFamily: {
+        "Poppins": "Poppins, sans-serif",
+      },
+      screens: {
+        "md-custom": "839px",
+      },
+    },
+  },
+  plugins: [],
+};
